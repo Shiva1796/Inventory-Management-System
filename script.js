538af9099cad1436238814b258900544fdd69b46
Shiva1796 <shiva96chawla@gmail.com>
2022-05-28 16:42:52 -0400

    Added font awesome icons

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
 "use strict";
-const partNumberHTML = document.getElementById("part-number");
-const quantityHTML = document.getElementById("quantity");
-const costHTML = document.getElementById("cost");
-const weightHTML = document.getElementById("weight");
-const finishHTML = document.getElementById("finish");
 const inventoryHTML = document.querySelectorAll(".data-inventory");
 const submitItem = document.getElementById("submitItem");
 const itemDisplay = document.querySelector(".display");
@@ -11,6 +6,11 @@ const viewToggle = document.querySelector("#view");
 
 //Function to generate new items from user input
 const newItems = function () {
+  const partNumberHTML = document.getElementById("part-number");
+  const quantityHTML = document.getElementById("quantity");
+  const costHTML = document.getElementById("cost");
+  const weightHTML = document.getElementById("weight");
+  const finishHTML = document.getElementById("finish");
   let items = {
     partNumber: partNumberHTML.value,
     quantity: Number(quantityHTML.value),
@@ -18,13 +18,10 @@ const newItems = function () {
     weight: Number(weightHTML.value),
     finish: finishHTML.value,
   };
-
   inventoryItems.push(items);
 };
 
 function displayItems() {
-  //Creating a current item variable to be used in specific tasks such as removing an item card
-
   itemDisplay.innerHTML = "";
   itemDisplay.length = inventoryItems.length;
   for (let i = 0; i < inventoryItems.length; i++) {
@@ -47,7 +44,7 @@ function displayItems() {
     itemDisplay.append(item);
     let remove = document.createElement("button");
     remove.setAttribute("id", "remove");
-    remove.innerHTML = "Remove";
+    remove.innerHTML = '<i class="fa-2x fa-solid fa-delete-left"></i>';
     item.append(remove);
 
     remove.addEventListener("click", function () {
@@ -63,27 +60,31 @@ viewToggle.addEventListener("click", function () {
 
 //Pressing the submit button will execute tasks
 submitItem.addEventListener("click", function () {
-  // inputLengthChecker();
-  newItems();
-  displayItems();
+  inputLengthChecker();
+  // newItems();
+  // displayItems();
 });
 
-//Function that checks wether the input fields are filled or not
-// function inputLengthChecker() {
-//   if (
-//     partNumberHTML.value == "" ||
-//     quantityHTML.value <= 0 ||
-//     costHTML.value <= 0 ||
-//     weightHTML.value <= 0
-//   ) {
-//     alert(
-//       "Please input all fields and make sure no values are negative and try again"
-//     );
-//   } else {
-//     newItems();
-//     displayItems();
-//   }
-// }
+// Function that checks wether the input fields are filled or not
+function inputLengthChecker() {
+  const partNumberHTML = document.getElementById("part-number");
+  const quantityHTML = document.getElementById("quantity");
+  const costHTML = document.getElementById("cost");
+  const weightHTML = document.getElementById("weight");
+  if (
+    partNumberHTML.value == "" ||
+    quantityHTML.value <= 0 ||
+    costHTML.value <= 0 ||
+    weightHTML.value <= 0
+  ) {
+    alert(
+      "Please input all fields and make sure no values are negative and try again"
+    );
+  } else {
+    newItems();
+    displayItems();
+  }
+}
 
 //InventoryItems is the array where different items will be stored in real time
 let inventoryItems = [];
